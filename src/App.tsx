36cd7f6974d7473974152c67f9caea312d36c009
atsu0203 <atsu0203@gmail.com>
2024-06-15 18:43:26 +0900

    Layputの修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { FC } from 'react'
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { BrowserRouter } from 'react-router-dom'
+// import { ClassicalFetchA } from './components/ClassicalFetchA'
+// import { ClassicalFetchB } from './components/ClassicalFetchB'
+// import { StateProvider } from './context/StateProvider'
+import Layout from './components/Layout'
+// import { ReactQueryA } from './components/ReactQueryA'
+// import { ReactQueryB } from './components/ReactQueryB'
+// import { MainContext } from './components/MainContext'
+// import { MainRTKit } from './components/MainRTKit'
 
-function App() {
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       retry: false,
+//       refetchOnWindowFocus: false,
+//     },
+//   },
+// })
+
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Layout>App</Layout>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
